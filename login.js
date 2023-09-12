35b530aebd40e97aa55203b107647ffdb0ec4841
amalalnaaimi <a97maaal@gmail.com>
2023-09-12 11:39:11 +0400

    mmm

---
+++ login.js
@@ -0,0 +1,8 @@
+function validate(){
+    var UesrName=document.getElementById("UesrName").Value;
+    var password=document.getElementById("PassWord").Value;
+    
+        location.href="home.html";
+
+   
+}
